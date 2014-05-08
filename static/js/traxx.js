aac11915ed4613d42448568a80572f1bb6b986e4
Josh Gachnang <josh@servercobra.com>
2014-05-07 21:12:18 -0700

    Fixing regex for links

--- static/js/traxx.js
+++ static/js/traxx.js
@@ -88,7 +88,8 @@ function chat_message(msg) {
 }
 
 function urlify(text) {
-    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var expression = /(^|\s)((https?:\/\/)?[\w-]+(\.[\w-]+)+\.?(:\d+)?(\/\S*)?)/gi;
+
     var regex = new RegExp(expression);
     var t = 'www.google.com';
     return text.replace(regex, function(url) {
